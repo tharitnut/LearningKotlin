2b8eddcc2c4e73d150899c35f034673fbd4b59e7
TharitNut <nong.peanut.butter.mail@gmail.com>
2025-08-18 02:27:16 +0700

    first commit

---
+++ MyKotlinApp/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.12.0"
+        id("org.jetbrains.kotlin.android") version "1.9.0" // or whatever Kotlin version you're using
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyKotlinApp"
+include(":app")
+ 
